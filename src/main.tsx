4d49dcfad45478c04e1d4e6f4575456dd6a19378
AsalaCraft <hamdaouihiba13@gmail.com>
2026-09-06 17:48:57 +0100

    Enable direct navigation from pack promotions to pack detail pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './pack-promos.css'
+import './pack-detail.css'
+import './pack-navigation.ts'
 import AppExact from './AppExact.tsx'
 
 createRoot(document.getElementById('root')!).render(
