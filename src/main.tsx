5fe0e4453612944f693b341501847a95910e2bed
AsalaCraft <hamdaouihiba13@gmail.com>
2026-09-12 15:04:54 +0100

    Load safe category photo fixes

--- src/main.tsx
+++ src/main.tsx
@@ -20,6 +20,7 @@ import './promo-safe.ts'
 import './category-art.ts'
 import './hero-safe.ts'
 import './client-logout.ts'
+import './presentation-visual-fixes.ts'
 import AppExact from './AppExact.tsx'
 
 createRoot(document.getElementById('root')!).render(
