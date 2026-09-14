09d14d129d5aa1944c5c7144399f60fca043de70
AsalaCraft <hamdaouihiba13@gmail.com>
2026-09-14 23:31:29 +0100

    Remove hero startup image mutation fix

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,6 @@ import './admin-auto-dashboard.ts'
 import './osrah-final-fixes.ts'
 import './client-enhancements.ts'
 import './pack-navigation.ts'
-import './hero-startup-fix.ts'
 
 import AppExact from './AppExact.tsx'
 
